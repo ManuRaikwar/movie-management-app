3e117c3a3556ced2836f3324376c85188f4202d5
Manu <manur830@gmail.com>
2026-03-22 00:58:35 +0530

    program.cs updated with sql connection and service registration

---
+++ API/MovieAccess.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieAccess.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieAccess.DataAccess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
